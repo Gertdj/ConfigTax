7882840e43857c74bbd10b1f1eea1ceab1a1c896
Gertdj <Gertdj@outlook.com>
2021-04-30 17:22:44 +0200

    sync

--- ConfigTaxWin/Form1.cs
+++ ConfigTaxWin/Form1.cs
@@ -18,7 +18,14 @@ namespace ConfigTaxWin
         public Form1()
         {
             InitializeComponent();
-        }
+        
+
+
+
+
+
+
+
 
         private void button1_Click(object sender, EventArgs e)
         {
